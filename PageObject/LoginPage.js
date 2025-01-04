646c08a496a2077d86e759f75be0392a0fc78383
admin <clavecillaslyn@gmail.com>
2025-01-04 23:58:09 +0800

    initial

--- PageObject/LoginPage.js
+++ PageObject/LoginPage.js
@@ -1,10 +1,17 @@
-
+const { expect } = require('@playwright/test');
 
 class LoginPage {
 
 constructor(page){
 
     this.page = page;
+    this.homepageurl = 'https://bpsweb-dev.azurewebsites.net/auth/sign-in';
+    this.homepagetitle = 'BizBox Profiling System';
+    this.username = '#email';
+    this.password = '#password';
+    this.showpassword = "button[aria-label='toggle password visibility'] svg";
+    this.rememberpass = "input[type='checkbox']";
+
 
 
 }
@@ -15,7 +22,23 @@ async navigatePage(){
 
 }
 
+async assertHomePage(){
 
+    await expect(this.page).toHaveURL(this.homepageurl);
+    await expect(this.page).toHaveTitle(this.homepagetitle);
+    
+    await expect(this.page.locator(this.password)).toBeVisible();
+    await expect(this.page.locator(this.showpassword)).toBeVisible();
+    await expect(this.page.locator(this.rememberpass)).toBeVisible();
+}
 
+async assertusername (){
+    await expect(this.page.locator(this.username)).toBeVisible();
+    await expect(this.page.locator(this.username)).toBeEnabled();
+}
+async enterusername (){
+    await this.page.fill(this.username, 'DevAccount');
+
+}
 }
 module.exports = LoginPage;
--- tests/Login_Existing_Account.spec.js
+++ tests/Login_Existing_Account.spec.js
@@ -3,7 +3,19 @@ const LoginPage = require('./../PageObject/LoginPage');
 test('Login Existing Account', async ({page}) => {
     const loginPage = new LoginPage(page);
 
+
+//Navigate home Page
 await loginPage.navigatePage();
+
+//Validate Home Page
+await loginPage.assertHomePage();
+console.log('Assert Home Page')
+
+//Validate Username
+await loginPage.assertusername();
+console.log('Assert Username')
+//Input Username
+await loginPage.enterusername();
 
 
 
