ac131d2e20bbc820f7399553018ad2de22cf2dd1
admin <clavecillaslyn@gmail.com>
2025-01-06 01:33:37 +0800

    inital

--- tests/Sessionsave.spec.js
+++ tests/Sessionsave.spec.js
@@ -5,11 +5,18 @@ test.beforeAll(async({browser}) =>
     
 {
 const context = await browser.newContext();
-const page = context.newPage();
+const page = await context.newPage();
 await page.goto('https://bpsweb-dev.azurewebsites.net/auth/sign-in');
-await page.fill('input[name="email"]','DevAccount');
-await page.fill('input[name="password"]','abcdE123');
-await page.click('text=Sign in');
+
 await context.storageState({path: 'State.json'})
+
+})
+
+test('Username', async({page}) => {
+    await page.fill('input[name="email"]','DevAccount');
+    await page.fill('input[name="password"]','abcdE123');
+    await page.click('text=Sign in');
+
+
 
 })
